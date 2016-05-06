aed571592775eb5211d3d55de1688b45feafdbd7
jerluc <jeremyalucas@gmail.com>
2016-05-06 10:13:47 -0700

    Adding some portable RC settings

--- new_setup.sh
+++ new_setup.sh
@@ -4,5 +4,8 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 ln -s $SCRIPTPATH/.vimrc $HOME/.vimrc
 ln -s $SCRIPTPATH/.vim $HOME/.vim
+ln -s $SCRIPTPATH/.portablerc $HOME/.portablerc
 
 exec $SCRIPTPATH/fetch_plugin_repos.sh
+
+echo 'source $HOME/.portablerc' >> $HOME/.bash_profile
